f6205f735a58f45e1811d32e0d8269670820d996
HuyHoang130405 <prosdmusic06@gmail.com>
2025-11-14 18:03:35 +0700

    chore: translate english to vietnamese

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -4,10 +4,10 @@ const authMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
-  if (!token) return res.status(401).json({ message: "No token provided" });
+  if (!token) return res.status(401).json({ message: "Bạn cần đăng nhập để thực hiện hành động này!" });
 
   jwt.verify(token, process.env.ACCESS_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Invalid or expired token" });
+    if (err) return res.status(403).json({ message: "Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại!" });
 
     req.user = decoded; // decoded phải chứa id của user
     next();
